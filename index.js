ac29b347a3405284a0e3101e3e143a0cbb4d4f4d
Ingrid G <ing.gape18@gmail.com>
2023-01-30 11:43:54 -0500

    funcion leer archivo

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+// import axios from 'axios';
 /* module.exports = () => {
   // ...
 }; */
 
+// Leer un archivo
+const fs = require('fs'); // requerimos el paquete file system que viene ya
+
+fs.readFile('./files/archive.md', 'utf8', (err, data) => { // encoding utf8 porque es archivo de texto
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
+
+// Averiguar la extensión de un archivo
 
