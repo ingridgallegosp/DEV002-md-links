f0153a92f3907e1bbc593b1f187ee50a9ead7503
Ingrid G <ing.gape18@gmail.com>
2023-02-02 09:12:42 -0500

    probando con promesas

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -11,17 +11,18 @@ const mdLinks = require('../index.js');
 // Test de Validacion de Path 
 describe('Tests para validar si ruta es absoluta', () => {
   const relativePath = './files/archive.md';
-  const absolutePath = './files/archive.md';
+  const absolutePath = 'C:/Users/INGRID/Desktop/Laboratoria/PROYECTO4-MDLINKS/DEV002-md-links/files/archive.md';
       
   it('debe validar que sea funcion', () => {
     expect(typeof validatePath).toBe('function');
+  });
+
+  it('debe mostrar false si la ruta dada no es absoluta',  () => {
+    expect(validatePath(relativePath)).toEqual(false);
   });
 
   it('debe mostrar true si la ruta dada  es absoluta',  () => {
     expect(validatePath(absolutePath)).toEqual(true);
   });
 
-  it('debe mostrar false si la ruta dada no es absoluta',  () => {
-    expect(validatePath(relativePath)).toEqual(false);
-  });
 });
